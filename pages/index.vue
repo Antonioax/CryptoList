3a5cc1c2a9d65e484255b483adf8c01943c38a60
Antonio Jurić <juricantonio18@gmail.com>
2024-08-15 20:49:21 +0200

    Created index page layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,19 @@
-<script setup></script>
+<script setup>
+const { data } = await useFetch("/api/tickers/?limit=10");
+</script>
 
 <template>
+  <main>
+    <h1>Index Page</h1>
+
+    <div>
+      <div v-for="currency in data.data" :key="data.data.id">
+        {{ currency.name }}
+        {{ currency.symbol }}
+        {{ currency.price_usd }}
+        <NuxtLink :to="'/currency/' + currency.id">{{ currency.id }}</NuxtLink>
+        <br />
+      </div>
+    </div>
+  </main>
 </template>
