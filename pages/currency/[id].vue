945814c0a14e883c9063941ac11df9e11048f599
Antonio Jurić <juricantonio18@gmail.com>
2024-08-15 21:23:19 +0200

    Created initial details page

--- pages/currency/[id].vue
+++ pages/currency/[id].vue
@@ -1,5 +1,15 @@
 <script setup>
+const route = useRoute();
+const { data } = await useFetch("/api/ticker/?id=" + route.params.id);
+const coin = data.value[0];
 </script>
 
 <template>
+  <div>
+    <div>Detail page</div>
+    <div>{{ coin.symbol }}</div>
+    <div>{{ coin.rank }}</div>
+    <div>{{ coin.price_usd }}</div>
+    <div>{{ coin.market_cap_usd }}</div>
+  </div>
 </template>
