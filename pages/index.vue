68cc02c79059dde9b29aa003b4561a70a40d771e
Antonio Jurić <juricantonio18@gmail.com>
2024-08-16 11:47:29 +0200

    Added initial index page design

--- pages/index.vue
+++ pages/index.vue
@@ -3,16 +3,20 @@ const { data } = await useFetch("/api/tickers/?limit=10");
 </script>
 
 <template>
-  <main>
-    <h1 class="bg-red-200">Index Page</h1>
+  <main class="flex flex-col justify-center items-center mt-20">
+    <h1 class="text-4xl">Crypto List</h1>
 
-    <div>
-      <div v-for="currency in data.data" :key="data.data.id">
-        {{ currency.name }}
-        {{ currency.symbol }}
-        {{ currency.price_usd }}
-        <NuxtLink :to="'/currency/' + currency.id">{{ currency.id }}</NuxtLink>
-        <br />
+    <div class="flex flex-col gap-1 mt-10 w-[500px] max-w-[90%]">
+      <div
+        v-for="(currency, index) in data.data"
+        :key="data.data.id"
+      >
+        <NuxtLink :to="'/currency/' + currency.id" class="flex hover:bg-emerald-200 py-2 sm:px-4 rounded-lg hover:shadow-xl">
+          <div class="w-6 text-right mr-2">{{ index + 1 }}.</div>
+          <div class="flex-1">{{ currency.name }}</div>
+          <div class="flex-1 text-center">{{ currency.symbol }}</div>
+          <div class="flex-1 text-right">${{ currency.price_usd }}</div>
+        </NuxtLink>
       </div>
     </div>
   </main>
