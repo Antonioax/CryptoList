f6380d26c3da395b9ada66d2ba3ba8b6611d9dba
Antonio Jurić <juricantonio18@gmail.com>
2024-08-16 13:03:33 +0200

    Updated design

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,4 +9,14 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
+  app: {
+    head: {
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000", // Example for Adobe Fonts
+        },
+      ],
+    },
+  },
 });
