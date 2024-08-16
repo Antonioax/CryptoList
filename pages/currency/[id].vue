aaf190b54e87aa572e1af05afb06a36dfb0de245
Antonio Jurić <juricantonio18@gmail.com>
2024-08-16 12:05:03 +0200

    Added initial details page design

--- pages/currency/[id].vue
+++ pages/currency/[id].vue
@@ -5,11 +5,64 @@ const coin = data.value[0];
 </script>
 
 <template>
-  <div>
-    <div>Detail page</div>
-    <div>{{ coin.symbol }}</div>
-    <div>{{ coin.rank }}</div>
-    <div>{{ coin.price_usd }}</div>
-    <div>{{ coin.market_cap_usd }}</div>
-  </div>
+  <main class="flex flex-col justify-center items-center mt-20">
+    <h1 class="text-4xl">{{ coin.name }}</h1>
+
+    <div class="flex flex-col gap-1 mt-10 w-full max-w-[90%]">
+      <div class="flex justify-between gap-3">
+        <div class="flex-1 text-right font-semibold">Symbol:</div>
+        <div class="flex-1 text-left">{{ coin.symbol }}</div>
+      </div>
+      <div class="flex justify-between gap-3">
+        <div class="flex-1 text-right font-semibold">Rank:</div>
+        <div class="flex-1 text-left">
+          {{
+            coin.rank +
+            (+coin.rank % 10 === 1
+              ? "st"
+              : +coin.rank % 10 === 2
+              ? "nd"
+              : +coin.rank % 10 === 3
+              ? "rd"
+              : "th")
+          }}
+        </div>
+      </div>
+      <div class="flex justify-between gap-3">
+        <div class="flex-1 text-right font-semibold">USD currency:</div>
+        <div class="flex-1 text-left">{{ coin.price_usd }}$</div>
+      </div>
+      <div
+        class="flex justify-between gap-3"
+        :class="
+          coin.percent_change_24h >= 0 ? 'text-emerald-600' : 'text-red-500'
+        "
+      >
+        <div class="flex-1 text-right font-semibold">Last 24h:</div>
+        <div class="flex-1 text-left">{{ coin.percent_change_24h }}%</div>
+      </div>
+      <div
+        class="flex justify-between gap-3"
+        :class="
+          coin.percent_change_1h >= 0 ? 'text-emerald-600' : 'text-red-500'
+        "
+      >
+        <div class="flex-1 text-right font-semibold">Last 1h:</div>
+        <div class="flex-1 text-left">{{ coin.percent_change_1h }}%</div>
+      </div>
+      <div
+        class="flex justify-between gap-3"
+        :class="
+          coin.percent_change_7d >= 0 ? 'text-emerald-600' : 'text-red-500'
+        "
+      >
+        <div class="flex-1 text-right font-semibold">Last 7d:</div>
+        <div class="flex-1 text-left">{{ coin.percent_change_7d }}%</div>
+      </div>
+      <div class="flex justify-between gap-3">
+        <div class="flex-1 text-right font-semibold">BTC:</div>
+        <div class="flex-1 text-left">{{ coin.price_btc }}BTC</div>
+      </div>
+    </div>
+  </main>
 </template>
